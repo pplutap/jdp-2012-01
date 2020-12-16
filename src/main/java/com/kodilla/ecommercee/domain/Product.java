5c354f7974dce054d4c7ed018f8a22036c1b74d5
BarcaLS <marcin@sarna.info>
2020-12-16 20:08:03 +0100

    removed unnecessary dependencies in Product class

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
