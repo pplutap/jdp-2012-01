2570f4d1d19ff82b05caa2ccfc458b9ffbc3c2f0
CezaryJanicki <cezary.janicki@gmail.com>
2020-12-16 23:18:52 +0100

    adding GroupController, GroupDto

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,39 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.GroupDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/group")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class GroupController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getGroups")
+    public List<GroupDto> getGroups() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createGroup", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public GroupDto createGroup(GroupDto groupDto) {
+        return new GroupDto(1L, "Test Group");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getGroupById")
+    public GroupDto getGroupById(long id) {
+        return new GroupDto(2L, "Test Group by Id");
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateGroup", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public GroupDto updateGroup(GroupDto groupDto) {
+        return new GroupDto(1L, "Edited test Group");
+    }
 }
