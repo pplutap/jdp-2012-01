868091ca8d775779ece8316a3d4272efaed741f9
marika <ms.czerwonka@gmail.com>
2020-12-19 05:38:15 +0100

    added UserDto

--- src/main/java/com/kodilla/ecommercee/domain/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/UserDto.java
@@ -1,4 +1,15 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserDto {
+
+    private Long id;
+    private String username;
+    private int status;
+    private int userKey;
+
 }
