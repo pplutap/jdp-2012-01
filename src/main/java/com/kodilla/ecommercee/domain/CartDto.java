7bfee02ffec2f2fcc1bda0854d3cec7fb1739ce2
CezaryJanicki <cezary.janicki@gmail.com>
2020-12-31 00:01:37 +0100

    Cart Entity implementation

--- src/main/java/com/kodilla/ecommercee/domain/CartDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/CartDto.java
@@ -3,11 +3,16 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+
 @Getter
 @AllArgsConstructor
 public class CartDto {
     private Long id;
     private String name;
+    private String description;
+    private BigDecimal price;
+    private String groupId;
 
     public void add(ProductDto productDto) {
 
