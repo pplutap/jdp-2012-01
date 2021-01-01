44cc19aea2664903f567b23d57381b8b58f9a72d
BarcaLS <marcin@sarna.info>
2021-01-01 14:31:15 +0100

    ProductController - real implementation

---
+++ src/main/java/com/kodilla/ecommercee/ProductNotFoundException.java
@@ -0,0 +1,4 @@
+package com.kodilla.ecommercee;
+
+public class ProductNotFoundException extends Exception {
+}
--- src/main/java/com/kodilla/ecommercee/service/DbService.java
+++ src/main/java/com/kodilla/ecommercee/service/DbService.java
@@ -1,4 +1,30 @@
 package com.kodilla.ecommercee.service;
+import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class DbService {
+    private final ProductRepository repository;
+
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    public Optional<Product> getProduct(final Long id) {
+        return repository.findById(id);
+    }
+
+    public Product saveProduct(final Product product) {
+        return repository.save(product);
+    }
+
+    public void deleteProduct(final Long userId) {
+        repository.deleteById(userId);
+    }
 }
