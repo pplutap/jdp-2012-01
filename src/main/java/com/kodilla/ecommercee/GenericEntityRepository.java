e523bfbafc5b084b7a551b229e576fba8a2e4781
marika <ms.czerwonka@gmail.com>
2021-01-05 02:26:21 +0100

    removed GenericEnity

--- src/main/java/com/kodilla/ecommercee/GenericEntityRepository.java
+++ src/main/java/com/kodilla/ecommercee/GenericEntityRepository.java
@@ -1,9 +1,8 @@
 package com.kodilla.ecommercee;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface GenericEntityRepository extends JpaRepository<GenericEntity, Long> {
+public interface GenericEntityRepository {
 }
