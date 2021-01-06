78302ca8e5023a0380a241925a16d4fc7a838606
BarcaLS <marcin@sarna.info>
2021-01-06 14:43:05 +0100

    added testUserRepositoryDeleteById and testUserRepositoryFindAll

--- src/test/java/com/kodilla/ecommercee/domain/UserRepositoryTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/UserRepositoryTestSuite.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -28,5 +29,46 @@ public class UserRepositoryTestSuite {
 
         //CleanUp
         userRepository.deleteById(id);
+    }
+
+    @Test
+    public void testUserRepositoryFindAll() {
+        //Given
+        User user1 = new User("Anonymous developer", 190, 13);
+        User user2 = new User("Kotlin programmer", 230, 7);
+
+        //When
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        //Then
+        Long id1 = user1.getId();
+        Long id2 = user2.getId();
+        List<User> listUsers = userRepository.findAll();
+        Assertions.assertEquals(2, listUsers.size());
+
+        //CleanUp
+        userRepository.deleteById(id1);
+        userRepository.deleteById(id2);
+    }
+
+    @Test
+    public void testUserRepositoryDeleteById() {
+        //Given
+        User user1 = new User("John Smith", 124, 71);
+        User user2 = new User("John Doe", 50, 18);
+
+        //When
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        //Then
+        Long id1 = user1.getId();
+        Long id2 = user2.getId();
+        userRepository.deleteById(id2);
+        Assertions.assertEquals(1, userRepository.findAll().size());
+
+        //CleanUp
+        userRepository.deleteById(id1);
     }
 }
