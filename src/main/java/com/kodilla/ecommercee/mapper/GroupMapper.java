0d099f8daef0143e908f2f96d8f64f4683a85d4b
Kamil Józefowski <k.m.jozefowski@gmail.com>
2021-01-09 21:41:29 +0100

    FIX GroupController - rzeczywista implementacja metod, Utwórz serwis, mapper i dto.

--- src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
@@ -10,22 +10,23 @@ import java.util.stream.Collectors;
 @Service
 public class GroupMapper {
 
-    public Group mapToGroup(final GroupDto groupDto){
+    public Group mapToGroup(final GroupDto groupDto) {
         return new Group(
                 groupDto.getId(),
                 groupDto.getName(),
-                null
+                groupDto.getProducts()
         );
     }
 
-    public GroupDto mapToGroupDto(final Group group){
+    public GroupDto mapToGroupDto(final Group group) {
         return new GroupDto(
                 group.getId(),
-                group.getName()
+                group.getName(),
+                group.getProducts()
         );
     }
 
-    public List<GroupDto> mapToGroupDtoList(final List<Group> groupList){
+    public List<GroupDto> mapToGroupDtoList(final List<Group> groupList) {
         return groupList.stream()
                 .map(this::mapToGroupDto)
                 .collect(Collectors.toList());
--- src/main/java/com/kodilla/ecommercee/service/DbService.java
+++ src/main/java/com/kodilla/ecommercee/service/DbService.java
@@ -1,15 +1,15 @@
 package com.kodilla.ecommercee.service;
 
+import com.kodilla.ecommercee.domain.Group;
 import com.kodilla.ecommercee.domain.Order;
+import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.domain.User;
+import com.kodilla.ecommercee.repository.GroupRepository;
 import com.kodilla.ecommercee.repository.OrderRepository;
+import com.kodilla.ecommercee.repository.ProductRepository;
 import com.kodilla.ecommercee.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import com.kodilla.ecommercee.domain.User;
-import com.kodilla.ecommercee.repository.UserRepository;
-import com.kodilla.ecommercee.domain.Product;
-import com.kodilla.ecommercee.repository.ProductRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +21,7 @@ public class DbService {
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
     private final OrderRepository orderRepository;
+    private final GroupRepository groupRepository;
 
 
     public List<User> getAllUsers() {
@@ -55,11 +56,36 @@ public class DbService {
         productRepository.deleteById(userId);
     }
 
-    public List<Order> getAllOrders() { return orderRepository.findAll(); }
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
 
-    public Optional<Order> getOrder(final Long id) { return orderRepository.findById(id); }
+    public Optional<Order> getOrder(final Long id) {
+        return orderRepository.findById(id);
+    }
 
-    public Order saveOrder(final Order order) {return orderRepository.save(order); }
+    public Order saveOrder(final Order order) {
+        return orderRepository.save(order);
+    }
 
-    public void deleteOrder(final Long orderId) { orderRepository.deleteById(orderId); }
+    public void deleteOrder(final Long orderId) {
+        orderRepository.deleteById(orderId);
+    }
+
+
+    public List<Group> getAllGroups() {
+        return groupRepository.findAll();
+    }
+
+    public Group saveGroup(final Group group) {
+        return groupRepository.save(group);
+    }
+
+    public Optional<Group> getGroup(final long id) {
+        return groupRepository.findById(id);
+    }
+
+    public void deleteGroup(final Long id) {
+        groupRepository.deleteById(id);
+    }
 }
