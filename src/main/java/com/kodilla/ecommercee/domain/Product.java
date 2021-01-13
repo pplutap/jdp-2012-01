4c5d597531530b04b69e50e607638ec69ce21c8e
Kamil Józefowski <k.m.jozefowski@gmail.com>
2021-01-13 01:09:28 +0100

    Test Encji Product

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -27,6 +27,12 @@ public class Product {
         this.groups = groups;
     }
 
+    public Product(String name, String description, Long price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
     @Id
     @GeneratedValue
     @NotNull
--- src/test/java/com/kodilla/ecommercee/domain/ProductRepositoryTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/ProductRepositoryTestSuite.java
@@ -1,14 +1,15 @@
 package com.kodilla.ecommercee.domain;
 
-import com.kodilla.ecommercee.repository.OrderRepository;
 import com.kodilla.ecommercee.repository.ProductRepository;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.Optional;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,9 +17,6 @@ public class ProductRepositoryTestSuite {
 
     @Autowired
     public ProductRepository productRepository;
-
-    @Autowired
-    public OrderRepository orderRepository;
 
     @Test
     public void testProductRepositorySave() {
@@ -30,7 +28,8 @@ public class ProductRepositoryTestSuite {
 
         //Then
         Long id = product.getId();
-        assertNotNull(id);
+        Optional<Product> readProduct = productRepository.findById(id);
+        Assertions.assertTrue(readProduct.isPresent());
 
         //CleanUp
         productRepository.deleteById(id);
